bdc93c065ee8d5b32ebde401c4b2dcf3edbda752
Stephen Sciortino <stephen@brooklyndata.co>
2023-04-13 17:33:59 -0400

    Add support for Snowflake

--- models/staging/stg_klaviyo__metrics.sql
+++ models/staging/stg_klaviyo__metrics.sql
@@ -6,6 +6,8 @@ with source as (
 
 , final as (
 
+{% if target.type == 'postgres'%}
+
     select
         id as metric_id
         , attributes::json->'name' as metric_name
@@ -13,10 +15,27 @@ with source as (
         , attributes::json->'integration'->'id' as integration_id
         , attributes::json->'integration'->'name' as integration_name
         , attributes::json->'integration'->'category' as integration_category
-        , updated as updated_at -- TODO Fix this in the tap
+        , updated as updated_at
+
+{% elif target.type == 'snowflake'%}
+
+    select
+        id as metric_id
+        , attributes:name::string as metric_name
+        , attributes:created::timestamptz as created_at
+        , attributes:integration:id::string as integration_id
+        , attributes:integration:name::string as integration_name
+        , attributes:integration:category::string as integration_category
+        , updated::timestamptz as updated_at
+
+{% else %}
+
+    select *
+
+{% endif %}
 
     from source
-
+    
 )
 
 select * from final
--- models/staging/stg_klaviyo__profiles.sql
+++ models/staging/stg_klaviyo__profiles.sql
@@ -6,6 +6,8 @@ with source as (
 
 , final as (
 
+{% if target.type == 'postgres'%}
+
     select
         id as profile_id
         , type as profile_type
@@ -45,7 +47,56 @@ with source as (
         , attributes::json->'subscriptions'->'email'->'marketing'->'method_detail' as email_marketing_method_detail
         , attributes::json->'subscriptions'->'email'->'marketing'->'list_suppressions' as email_marketing_list_suppressions
         , attributes::json->'subscriptions'->'email'->'marketing'->'custom_method_detail' as email_marketing_custom_method_detail
-        , updated as updated_at -- TODO Fix this in the tap
+        , updated as updated_at
+
+{% elif target.type == 'snowflake'%}
+
+    select
+        id as profile_id
+        , type as profile_type
+        , attributes:email::string as email
+        , attributes:image::string as image
+        , attributes:title::string as title
+        , attributes:created::timestamptz as created_at
+        , attributes:first_name::string as first_name
+        , attributes:last_name::string as last_name
+        , attributes:properties::object as properties
+        , attributes:external_id::string as external_id
+        , attributes:organization::string as organization
+        , attributes:phone_number::string as phone_number
+        , attributes:last_event_date::timestamptz as last_event_date
+        , attributes:location:zip::string as zip_code
+        , attributes:location:city::string as city
+        , attributes:location:region::string as region
+        , attributes:location:country::string as country
+        , attributes:location:address1::string as address1
+        , attributes:location:address2::string as address2
+        , attributes:location:latitude::string as latitude
+        , attributes:location:timezone::string as timezone
+        , attributes:location:longitude::string as longitude
+        , attributes:subscriptions:sms:marketing:method::string as sms_marketing_method
+        , attributes:subscriptions:sms:marketing:consent::string as sms_marketing_consent
+        , attributes:subscriptions:sms:marketing:timestamp::timestamptz as sms_marketing_timestampt
+        , attributes:subscriptions:sms:marketing:double_optin::string as sms_marketing_double_optin
+        , attributes:subscriptions:sms:marketing:suppressions::string as sms_marketing_suppressions
+        , attributes:subscriptions:sms:marketing:method_detail::string as sms_marketing_method_detail
+        , attributes:subscriptions:sms:marketing:list_suppressions::string as sms_marketing_list_suppressions
+        , attributes:subscriptions:sms:marketing:custom_method_detail::string as sms_marketing_custom_method_detail
+        , attributes:subscriptions:email:marketing:method::string as email_marketing_method
+        , attributes:subscriptions:email:marketing:consent::string as email_marketing_consent
+        , attributes:subscriptions:email:marketing:timestamp::timestamptz as email_marketing_timestampt
+        , attributes:subscriptions:email:marketing:double_optin::string as email_marketing_double_optin
+        , attributes:subscriptions:email:marketing:suppressions::array as email_marketing_suppressions
+        , attributes:subscriptions:email:marketing:method_detail::string as email_marketing_method_detail
+        , attributes:subscriptions:email:marketing:list_suppressions::array as email_marketing_list_suppressions
+        , attributes:subscriptions:email:marketing:custom_method_detail::string as email_marketing_custom_method_detail
+        , updated as updated_at
+
+{% else %}
+
+    select *
+
+{% endif %}
 
     from source
 
