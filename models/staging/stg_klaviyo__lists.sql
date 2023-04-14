d73b110944ca4e00462588df9fdc163a1fc6614f
Stephen Sciortino <stephen@brooklyndata.co>
2023-04-14 16:39:12 -0400

    Update lists with Snowflake syntax

--- models/staging/stg_klaviyo__lists.sql
+++ models/staging/stg_klaviyo__lists.sql
@@ -6,11 +6,27 @@ with source as (
 
 , final as (
 
+{% if target.type == 'postgres'%}
+
     select
         id as event_id
         , attributes::json->'name' as list_name
         , attributes::json->'created' as created_at
         , updated as updated
+
+{% elif target.type == 'snowflake'%}
+
+    select
+        id::string as event_id
+        , attributes:name::string as list_name
+        , attributes:created::timestamptz as created_at
+        , updated::timestamptz as updated
+
+{% else %}
+
+    select *
+
+{% endif %}
 
     from source
 
