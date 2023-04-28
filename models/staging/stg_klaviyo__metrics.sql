6218e17bb84449c0ce2e4a2f754bff355d54f4c3
Stephen Sciortino <stephen@brooklyndata.co>
2023-04-28 09:50:58 -0400

    Refactor models for Snowflake only

--- models/staging/stg_klaviyo__metrics.sql
+++ models/staging/stg_klaviyo__metrics.sql
@@ -6,19 +6,6 @@ with source as (
 
 , final as (
 
-{% if target.type == 'postgres'%}
-
-    select
-        id as metric_id
-        , attributes::json->'name' as metric_name
-        , attributes::json->'created' as created_at
-        , attributes::json->'integration'->'id' as integration_id
-        , attributes::json->'integration'->'name' as integration_name
-        , attributes::json->'integration'->'category' as integration_category
-        , updated as updated
-
-{% elif target.type == 'snowflake'%}
-
     select
         id as metric_id
         , attributes:name::string as metric_name
@@ -27,13 +14,6 @@ with source as (
         , attributes:integration:name::string as integration_name
         , attributes:integration:category::string as integration_category
         , updated::timestamptz as updated
-
-{% else %}
-
-    select *
-
-{% endif %}
-
     from source
 
 )
--- models/staging/stg_klaviyo__profiles.sql
+++ models/staging/stg_klaviyo__profiles.sql
@@ -6,51 +6,6 @@ with source as (
 
 , final as (
 
-{% if target.type == 'postgres'%}
-
-    select
-        id as profile_id
-        , type as profile_type
-        , attributes::json->'email' as email
-        , attributes::json->'image' as image
-        , attributes::json->'title' as title
-        , attributes::json->'created' as created
-        , attributes::json->'first_name' as first_name
-        , attributes::json->'last_name' as last_name
-        , attributes::json->'properties' as properties
-        , attributes::json->'external_id' as external_id
-        , attributes::json->'organization' as organization
-        , attributes::json->'phone_number' as phone_number
-        , attributes::json->'last_event_date' as last_event_date
-        , attributes::json->'location'->'zip' as zip
-        , attributes::json->'location'->'city' as city
-        , attributes::json->'location'->'region' as region
-        , attributes::json->'location'->'country' as country
-        , attributes::json->'location'->'address1' as address1
-        , attributes::json->'location'->'address2' as address2
-        , attributes::json->'location'->'latitude' as latitude
-        , attributes::json->'location'->'timezone' as timezone
-        , attributes::json->'location'->'longitude' as longitude
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'method' as sms_marketing_method
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'consent' as sms_marketing_consent
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'timestamp' as sms_marketing_timestampt
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'double_optin' as sms_marketing_double_optin
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'suppressions' as sms_marketing_suppressions
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'method_detail' as sms_marketing_method_detail
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'list_suppressions' as sms_marketing_list_suppressions
-        , attributes::json->'subscriptions'->'sms'->'marketing'->'custom_method_detail' as sms_marketing_custom_method_detail
-        , attributes::json->'subscriptions'->'email'->'marketing'->'method' as email_marketing_method
-        , attributes::json->'subscriptions'->'email'->'marketing'->'consent' as email_marketing_consent
-        , attributes::json->'subscriptions'->'email'->'marketing'->'timestamp' as email_marketing_timestampt
-        , attributes::json->'subscriptions'->'email'->'marketing'->'double_optin' as email_marketing_double_optin
-        , attributes::json->'subscriptions'->'email'->'marketing'->'suppressions' as email_marketing_suppressions
-        , attributes::json->'subscriptions'->'email'->'marketing'->'method_detail' as email_marketing_method_detail
-        , attributes::json->'subscriptions'->'email'->'marketing'->'list_suppressions' as email_marketing_list_suppressions
-        , attributes::json->'subscriptions'->'email'->'marketing'->'custom_method_detail' as email_marketing_custom_method_detail
-        , updated as updated
-
-{% elif target.type == 'snowflake'%}
-
     select
         id as profile_id
         , type as profile_type
@@ -91,13 +46,6 @@ with source as (
         , attributes:subscriptions:email:marketing:list_suppressions::array as email_marketing_list_suppressions
         , attributes:subscriptions:email:marketing:custom_method_detail::string as email_marketing_custom_method_detail
         , updated::timestamptz as updated
-
-{% else %}
-
-    select *
-
-{% endif %}
-
     from source
 
 )
